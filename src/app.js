c3801782b7cef8a4509ccd23f04e8e1631184843
alangak <alanga@live.com>
2025-04-05 23:29:17 +0530

    Empty

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@
 import white from "chalk";
 import boxen from "boxen";
 
-const greeting = white.bold("Hello World !");
+const greeting = white.bold("Hello World ! Late Done");
 
 const boxenOptions = {
  padding: 1,
  margin: 1,
  borderStyle: "round",
- borderColor: "yellow",
+ borderColor: "green",
  backgroundColor: "#555555"
 };
 const msgBox = boxen( greeting, boxenOptions );
